dd73300e0d08e3f00d49f31e6429e56b622383b9
Tanjin <NamelessKing@users.noreply.github.com>
2026-02-20 14:35:47 +0100

    first commit

---
+++ src/main/java/it/epicode/gestioneeventi/EpicodeS7SpringWeekProjectGestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.gestioneeventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeS7SpringWeekProjectGestioneEventiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicodeS7SpringWeekProjectGestioneEventiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/epicode/gestioneeventi/EpicodeS7SpringWeekProjectGestioneEventiTests.java
@@ -0,0 +1,13 @@
+package it.epicode.gestioneeventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeS7SpringWeekProjectGestioneEventiTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
